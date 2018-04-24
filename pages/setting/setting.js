506ede3ff4c2fe4c713f873a3795ef0385560d85
sunlu <cfabregas@163.com>
2018-04-24 22:38:50 +0800

    fixed

--- pages/setting/setting.js
+++ pages/setting/setting.js
@@ -1,19 +1,26 @@
 import util from '../../utils/util'
 const app = getApp()
+const listType = {
+  grid: '九宫格',
+  list: '列表'
+}
+const theme = {
+  default: '默认'
+}
 
 Page({
   data: {
-    avatar: '../../assets/avatar.png',
-    name: '',
-    locale: ''
+    avatar: '../../assets/avatar.png'
   },
   onReady () {
     const user = app.Store.userInfo
     if (user.avatarUrl) {
       this.setData({
         avatar: user.avatarUrl,
-        name: user.nickName || '游客',
-        locale: `${user.country} ${user.province} ${user.city}`
+        name: user.nickName || '不愿意透露姓名的玩家',
+        locale: user.country ? `${user.country} ${user.province} ${user.city}` : '',
+        listType: listType[user.list_type],
+        theme: theme[user.theme]
       })
     }
   },
@@ -24,5 +31,17 @@ Page({
         urls: [this.data.avatar]
       })
     }
+  },
+  onCellTap (e) {
+    console.log(e.currentTarget.id, top)
+    wx.showActionSheet({
+      itemList: ['23', '34'],
+      success: function(res) {
+        console.log(res.tapIndex)
+      },
+      fail: function(res) {
+        console.log(res.errMsg)
+      }
+    })
   }
 })
